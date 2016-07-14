ad7c8b94c25b27bf64ed9de303da51dae4123700
ScottWoodbyrne <scott.woodbyrne@gmail.com>
2016-07-14 16:36:25 +0100

    update

---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+import views
+
+urlpatterns = [
+    url(r'^register/$', views.register, name='register'),
+    url(r'^profile/$', views.profile, name='profile'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^logout/$', views.logout, name='logout'),
+]
